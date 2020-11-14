dab5b85ca8d7829e165780a39d3d2a810966ced8
Alhassan Faisal <alhassanfgh@gmail.com>
2020-11-14 02:15:13 +0000

    Continous integration

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ export class Log {
     private executeLogs = (message: string, contextualData: any, async: boolean = false, type: string) => {
     var date = new Date();
     var logDate = date.getFullYear() + "-" + ('0' + (date.getMonth() + 1)).slice(-2) + "-" + ('0' + date.getDate()).slice(-2) + " " + ('0' + date.getHours()).slice(-2) + ":" + ('0' + date.getMinutes()).slice(-2) + ":" + ('0' + date.getSeconds()).slice(-2);
-
+    
     if(async){
       fs.readFile(`${storagePath}/app.log`, 'utf8', (error, read) => {
         const newLog = `${read !== '' ? `${read}\n` : read}[${logDate}] ${process.env.NODE_ENV || 'local'}.${type}: ${message ? message : ''} ${contextualData ? (typeof contextualData === 'object' && contextualData !== null ? JSON.stringify(contextualData) : contextualData) : ''}`;
@@ -46,11 +46,8 @@ export class Log {
           fs.openSync(`${storagePath}/app.log`, 'w');
         }
       }
-
       const data = fs.readFileSync(`${storagePath}/app.log`, 'utf8');
-      
       const newLog = `${data !== '' ? `${data}\n` : data}[${logDate}] ${process.env.NODE_ENV || 'local'}.${type}: ${message ? message : ''} ${contextualData ? (typeof contextualData === 'object' && contextualData !== null ? JSON.stringify(contextualData) : contextualData) : ''}`
-      
       fs.writeFileSync(`${storagePath}/app.log`, newLog);
     }
   }
